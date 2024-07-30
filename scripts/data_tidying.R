744a417eaea40af6338e89985cdea259009f908b
hannaaarvoldriley <cuj22dju@uea.ac.uk>
2024-07-30 17:16:39 +0000

    set up 3 different scripts to keep code organised. started to plan how i will tidy data using #, and sourced my tidy script to my exp. analysis script ready for when i get to analysis

---
+++ scripts/data_tidying.R
@@ -0,0 +1,21 @@
+# personal access token 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 (delete before submitting)
+
+# packages ----
+#add packages as necessary
+
+# data ----
+#read in raw data
+
+# tidying ----
+#head
+
+#colnames
+
+#clean colnames?
+
+#rename colnames? (glimpse to check if worked)
+
+#check na
+
+#check duplications (remove duplicates if necessary)
+
---
+++ scripts/exploratory_analysis.R
@@ -0,0 +1,5 @@
+# clear environment ----
+rm(list=ls())
+
+# source tidy data script ----
+source("scripts/data_tidying.R")
