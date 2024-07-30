548f682d0925d790ab62a32d253c3b9a5f0d1018
hannaaarvoldriley <cuj22dju@uea.ac.uk>
2024-07-30 19:22:31 +0000

    data cleaning - column names, checking for and removing typos, checking for missing values and duplications

--- scripts/data_tidying.R
+++ scripts/data_tidying.R
@@ -5,6 +5,7 @@
 #add packages as necessary
 library(tidyverse) # tidy data packages
 library(janitor) # clean data names
+library(dplyr) # data manipulation
 
 # import data ----
 butterflies <- read_csv("data/inbreeding_butterfly.csv")
@@ -27,8 +28,49 @@ butterflies <- rename(butterflies,
 glimpse(butterflies) #check new column names
 
 
+## rename text values ----
 
-#check na
+butterflies <- butterflies %>%
+  mutate(body_part=case_when(body_part == "drythor" ~ "thorax",
+                             body_part == "dryrest" ~ "rest",
+                             body_part == "drytotal" ~ "total")) #remove unnecessary characters from text values to make more readable
 
-#check duplications (remove duplicates if necessary)
+
+## check for errors in data ----
+
+butterflies %>%
+  duplicated() %>%
+  sum() #check for duplicated data
+
+
+butterflies %>%
+  is.na() %>%
+  sum() #check for missing data
+
+
+
+butterflies %>%
+  summarise(min=min(flight_inhibit_idx, na.rm=TRUE), 
+            max=max(flight_inhibit_idx, na.rm=TRUE)) #check for typos in fii
+#one individual's fii is 660 which seems unlikely for a 2 minute period
+#most likely meant to be 66 as this is within the range of other fiis
+#i will remove to be safe
+butterflies <- butterflies %>%
+  filter(flight_inhibit_idx != "660") #filters out the row with the error
+
+butterflies %>%
+  group_by(body_part) %>%
+  summarise(min=min(weight_mg, na.rm=TRUE),
+            max=max(weight_mg, na.rm=TRUE)) #check for typos in weights of body parts
+
+print(butterflies %>% 
+        distinct(inbreed_coeff))#check for typos in inbreeding coefficient (there should only be 3 levels of ic)
+
+print(butterflies %>% 
+        distinct(body_part)) #check for typos in body parts
+
+#make all values 2 decimal places?
+
+
+
 
