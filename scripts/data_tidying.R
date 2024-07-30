4b274a4911c9dbfbadeb0e652f46222e07b2dba9
hannaaarvoldriley <cuj22dju@uea.ac.uk>
2024-07-30 17:51:14 +0000

    read in data. cleaned column names.

--- scripts/data_tidying.R
+++ scripts/data_tidying.R
@@ -1,19 +1,32 @@
 # personal access token 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 (delete before submitting)
+# title (change)----
 
 # packages ----
 #add packages as necessary
+library(tidyverse) # tidy data packages
+library(janitor) # clean data names
 
-# data ----
-#read in raw data
+# import data ----
+butterflies <- read_csv("data/inbreeding_butterfly.csv")
 
 # tidying ----
-#head
 
-#colnames
+head(butterflies) #check data has loaded
 
-#clean colnames?
+## column names ----
 
-#rename colnames? (glimpse to check if worked)
+colnames(butterflies) #view column names
+
+butterflies <- clean_names(butterflies) #change column names to snake_case
+
+butterflies <- rename(butterflies,
+                      "inbreed_coeff" = "ic",
+                      "flight_inhibit_idx" = "fii",
+                      "weight_mg" = "dry_weight") #rename column names to be more informative while still concise
+
+glimpse(butterflies) #check new column names
+
+
 
 #check na
 
