c398f371474b175500810bb05011a7375c44178e
hannaaarvoldriley <cuj22dju@uea.ac.uk>
2024-07-31 20:03:23 +0000

    a few quick plots to look for any visible correlations

--- scripts/exploratory_analysis.R
+++ scripts/exploratory_analysis.R
@@ -1,5 +1,34 @@
 # clear environment ----
 rm(list=ls())
 
+# packages ----
+
+
 # source tidy data script ----
 source("scripts/data_tidying.R")
+
+ggplot(data = butterflies_tidy,
+       aes(x = inbreed_coeff,
+           y = flight_inhibit_idx))+
+  geom_jitter() #ic vs fii - appears to be a correlation
+
+
+
+ggplot(data = butterflies_tidy, 
+       aes(x = inbreed_coeff, 
+           y = weight_mg, 
+           color = body_part)) +
+  geom_jitter() #ic vs weight - no obvious correlation
+
+ggplot(data = butterflies_tidy, 
+       aes(x = weight_mg, 
+           color = body_part, 
+           y = flight_inhibit_idx)) +
+  geom_jitter() #weight vs fii - no obvious correlation
+
+filtered_data <- butterflies_tidy %>% 
+  filter(body_part == "total")
+ggplot(data = filtered_data, 
+       aes(x = weight_mg, 
+           y = flight_inhibit_idx)) +
+  geom_jitter() #tried filtering total, thorax and rest of body to check no correlations between any of those and fii
