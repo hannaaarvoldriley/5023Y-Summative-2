d642722f00fe37395e647a789b147fc62bb7768f
hannaaarvoldriley <cuj22dju@uea.ac.uk>
2024-08-01 17:42:19 +0000

    pivoted data to wide (required reworking most of tidying script)

--- scripts/data_tidying.R
+++ scripts/data_tidying.R
@@ -11,34 +11,31 @@ library(dplyr) # data manipulation
 # import data ----
 butterflies <- read_csv("data/inbreeding_butterfly.csv")
 
+head(butterflies) #check data has loaded
+
 # tidying ----
 
-head(butterflies) #check data has loaded
+## pivot data to wide format ---
+#individuals only have 1 row each which makes fitting lm easier
+butterflies_wide <- pivot_wider(data = butterflies,
+                                names_from = body_part,
+                                values_from = dry_weight)
+
 
 ## column names ----
 
-colnames(butterflies) #view column names
+colnames(butterflies_wide) #view column names
 
-butterflies_tidy <- clean_names(butterflies) #change column names to snake_case
+butterflies_tidy <- clean_names(butterflies_wide) #change column names to snake_case
 
 butterflies_tidy <- rename(butterflies_tidy,
                       "inbreed_coeff" = "ic",
                       "flight_inhibit_idx" = "fii",
-                      "weight_mg" = "dry_weight") #rename column names to be more informative while still concise
+                      "thorax_mg" = "drythor",
+                      "rest_mg" = "dryrest",
+                      "total_mg" = "drytotal") #rename column names to be informative and concise
 
 glimpse(butterflies_tidy) #check new column names
-
-## data format ----
-
-butterflies_tidy <- butterflies_tidy %>%
-  mutate(inbreed_coeff = as.factor(inbreed_coeff)) #treat inbreeding coefficient as a factor instead of numeric
-
-## rename text values ----
-
-butterflies_tidy <- butterflies_tidy %>%
-  mutate(body_part=case_when(body_part == "drythor" ~ "thorax",
-                             body_part == "dryrest" ~ "rest",
-                             body_part == "drytotal" ~ "total")) #remove unnecessary characters from text values to make more readable
 
 
 ## check for errors in data ----
@@ -57,22 +54,23 @@ butterflies_tidy %>%
   summarise(min=min(flight_inhibit_idx, na.rm=TRUE), 
             max=max(flight_inhibit_idx, na.rm=TRUE)) #check for typos in fii
 butterflies_tidy <- butterflies_tidy %>%
-  filter(flight_inhibit_idx != "660") #filter out row with error
-#can rerun previous line to check
+  filter(flight_inhibit_idx != "660") #filter out row with error in fii
+
 
 butterflies_tidy %>%
-  group_by(body_part) %>%
-  summarise(min=min(weight_mg, na.rm=TRUE),
-            max=max(weight_mg, na.rm=TRUE)) #check for typos in weights of body parts
+  summarise(min=min(total_mg, na.rm=TRUE),
+            max=max(total_mg, na.rm=TRUE)) #check for typos in weights of body parts
+butterflies_tidy %>%
+  summarise(min=min(thorax_mg, na.rm=TRUE),
+            max=max(thorax_mg, na.rm=TRUE))
+butterflies_tidy %>%
+  summarise(min=min(rest_mg, na.rm=TRUE),
+            max=max(rest_mg, na.rm=TRUE))
 
 print(butterflies_tidy %>% 
         distinct(inbreed_coeff))#check for typos in inbreeding coefficient (there should only be 3 levels of ic)
 
-print(butterflies_tidy %>% 
-        distinct(body_part)) #check for typos in body parts
-
 #make all values 2 decimal places?
-
 
 
 
